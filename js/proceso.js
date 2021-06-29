5b41ba8e390c3f2b749f8389dd7a40d12ae5afe1
Jonathan Mendoza <jonathanmendoza281@hotmail.com>
2021-06-28 21:08:23 -0500

    nalda

--- js/proceso.js
+++ js/proceso.js
@@ -22,24 +22,3 @@ telefono.addEventListener('keyup', (e) => {
 	// Eliminar las letras
 	.replace(/\D/g, '');
 } );
-direccion.addEventListener('keyup', (e) =>{
-    let valorInput=e.target.value;
-    //Eliminamos los numeros
-    direccion.value = valorInput.replace(/[0-9]/g, '');
-    direccion.textContent = valorInput;
-});
-formulario.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    if(nombre.value && apellidos.value && telefono.value && direccion.value && correo.value && ref_dire.value !==''){
-        formulario.reset();
-        document.getElementById('form_enviado').classList.add('form_enviado-activo');
-        setTimeout(() => {
-			document.getElementById('form_enviado').classList.remove('form_enviado-activo');
-    }, 5000);}
-    else{document.getElementById('form_error').classList.add('form_error-activo');
-    setTimeout(() => {
-        document.getElementById('form_error').classList.remove('form_error-activo');
-}, 500);}
-
-});
